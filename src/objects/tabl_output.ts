02c3360864dc61996779207d7a46c9da67759e12
Lars Hvam <larshp@hotmail.com>
2021-12-13 18:06:19 +0100

    fixes

--- src/objects/tabl_output.ts
+++ src/objects/tabl_output.ts
@@ -6,15 +6,22 @@ export class TABLOutput {
     const type = obj.parseType(reg);
     if (type instanceof abaplint.BasicTypes.StructureType) {
       ret += `<table border="1">\n`;
-      ret += `<tr><td><u>Field</u></td><td></td><td></td></tr>\n`;
+      ret += `<tr><td><u>Field</u></td><td>DDIC</td><td>Type</td></tr>\n`;
       for (const f of type.getComponents()) {
         let text = f.type.toText(0);
-        if (f instanceof abaplint.BasicTypes.StructureType) {
+        if (f.type instanceof abaplint.BasicTypes.StructureType) {
           text = "Structure";
-        } else if (f instanceof abaplint.BasicTypes.TableType) {
+        } else if (f.type instanceof abaplint.BasicTypes.TableType) {
           text = "Table";
         }
-        ret += `<tr><td>${f.name}</td><td>${f.type.getQualifiedName() || ""}</td><td>${text}</td></tr>\n`;
+        let ddicName = f.type.getQualifiedName() || "";
+        if (ddicName.startsWith(obj.getName() + "-")) {
+          ddicName = "";
+        }
+        if (ddicName !== "") {
+          ddicName = `<a href="todo">${ddicName}</a>`;
+        }
+        ret += `<tr><td>${f.name}</td><td>${ddicName}</td><td>${text}</td></tr>\n`;
       }
       ret += `</table>\n`;
     }
