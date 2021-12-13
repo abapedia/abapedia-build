b546af3d927232035e3f600c34c3c205145e316f
Lars Hvam <larshp@hotmail.com>
2021-12-13 18:18:08 +0100

    fixes (#4)
    * wip
    
    * fix
    
    * fixes
    
    * fix

--- src/objects/dtel_output.ts
+++ src/objects/dtel_output.ts
@@ -4,8 +4,11 @@ export class DTELOutput {
   public output(object: abaplint.Objects.DataElement, reg: abaplint.IRegistry): string {
     let ret = object.getType() + " " + object.getName() + "<br>\n";
     const parsed = object.parseType(reg);
-    ret += parsed.toText(0) + "<br>\n";
-    ret += parsed.getQualifiedName();
+    if (object.getDomainName() !== undefined && object.getDomainName() !== "") {
+      ret += `<a href="todo">` + object.getDomainName() + "</a><br>\n";
+    } else {
+      ret += parsed.toText(0) + "<br>\n";
+    }
     return ret;
   }
 }
--- src/objects/tabl_output.ts
+++ src/objects/tabl_output.ts
@@ -6,9 +6,22 @@ export class TABLOutput {
     const type = obj.parseType(reg);
     if (type instanceof abaplint.BasicTypes.StructureType) {
       ret += `<table border="1">\n`;
-      ret += `<tr><td><u>Field</u></td><td></td><td></td></tr>\n`;
+      ret += `<tr><td><u>Field</u></td><td><u>DDIC</u></td><td><u>Type</u></td></tr>\n`;
       for (const f of type.getComponents()) {
-        ret += `<tr><td>${f.name}</td><td>${f.type.getQualifiedName()}</td><td>${f.type.toText(0)}</td></tr>\n`;
+        let text = f.type.toText(0);
+        if (f.type instanceof abaplint.BasicTypes.StructureType) {
+          text = "Structure";
+        } else if (f.type instanceof abaplint.BasicTypes.TableType) {
+          text = "Table";
+        }
+        let ddicName = f.type.getQualifiedName() || "";
+        if (ddicName.startsWith(obj.getName() + "-")) {
+          ddicName = "";
+        }
+        if (ddicName !== "") {
+          ddicName = `<a href="todo">${ddicName}</a>`;
+        }
+        ret += `<tr><td>${f.name}</td><td>${ddicName}</td><td>${text}</td></tr>\n`;
       }
       ret += `</table>\n`;
     }
