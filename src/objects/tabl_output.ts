72500c4edc32b498035d80945dbac23da5ca4d94
Lars Hvam <larshp@hotmail.com>
2021-12-13 17:48:24 +0100

    fix

--- src/objects/tabl_output.ts
+++ src/objects/tabl_output.ts
@@ -14,7 +14,7 @@ export class TABLOutput {
         } else if (f instanceof abaplint.BasicTypes.TableType) {
           text = "Table";
         }
-        ret += `<tr><td>${f.name}</td><td>${f.type.getQualifiedName()}</td><td>${text}</td></tr>\n`;
+        ret += `<tr><td>${f.name}</td><td>${f.type.getQualifiedName() || ""}</td><td>${text}</td></tr>\n`;
       }
       ret += `</table>\n`;
     }
