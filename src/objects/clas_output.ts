1365d57c784b3df8a9bf50ef6daf1a4556fbf113
Lars Hvam <larshp@hotmail.com>
2021-12-21 11:38:44 +0100

    add superclass

--- src/objects/clas_output.ts
+++ src/objects/clas_output.ts
@@ -1,9 +1,15 @@
 import * as abaplint from "@abaplint/core";
-import { outputDefinition } from "./_helpers";
+import { objectLink, outputDefinition } from "./_helpers";
 
 export class CLASOutput {
   public output(object: abaplint.Objects.Class): string {
     let ret = "";
+
+    const def = object.getClassDefinition();
+    if (def?.superClassName) {
+      ret += "Superclass: " + objectLink("CLAS", def.superClassName) + "<br>";
+    }
+
     ret += outputDefinition(object.getDefinition());
     return ret;
   }
