12c00c413a73943e6742895424b68e56241c8e1a
Lars Hvam <larshp@hotmail.com>
2021-12-29 13:16:29 +0100

    as table

--- src/objects/ddls_output.ts
+++ src/objects/ddls_output.ts
@@ -4,9 +4,15 @@ export class DDLSOutput {
   public output(object: abaplint.Objects.DataDefinition): string {
     let ret = "";
 
+    ret += `<table border="1">\n`;
+    ret += `<tr>
+    <td><u>Key</u></td>
+    <td><u>Field</u></td>
+    </tr>\n`;
     for (const f of object.getParsedData()?.fields || []) {
-      ret += f.key + ", " + f.name + "<br>";
+      ret += "<tr><td>" + (f.key === true ? "KEY" : "") + "</td><td><tt>" + f.name + "</tt></td></tr>\n";
     }
+    ret += `</table>\n`;
 
     return ret;
   }
