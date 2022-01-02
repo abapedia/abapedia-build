93c529dc9aa915577b632c3a21452632032141c3
Lars Hvam <larshp@hotmail.com>
2022-01-02 11:27:34 +0100

    types and esceptions

--- src/objects/_helpers.ts
+++ src/objects/_helpers.ts
@@ -1,5 +1,22 @@
 import * as abaplint from "@abaplint/core";
 
+function typeText(id: abaplint.TypedIdentifier): string {
+  const type = id.getType();
+  let typeText = "";
+  if (type instanceof abaplint.BasicTypes.CSequenceType) {
+    typeText = "csequence";
+  } else if (type instanceof abaplint.BasicTypes.CLikeType) {
+    typeText = "clike";
+  } else if (type instanceof abaplint.BasicTypes.XSequenceType) {
+    typeText = "xsequence";
+  } else if (type instanceof abaplint.BasicTypes.NumericGenericType) {
+    typeText = "numeric";
+  } else {
+    typeText = type.getQualifiedName() !== undefined ? type.getQualifiedName()! : type.toABAP();
+  }
+  return typeText;
+}
+
 function methodParameters(m: abaplint.IMethodDefinition): string {
   let ret = "";
 
@@ -7,18 +24,25 @@ function methodParameters(m: abaplint.IMethodDefinition): string {
 
   for (const p of m.getParameters().getImporting()) {
     const opt: string = optional.some(a => a.toUpperCase() === p.getName().toUpperCase()) ? " Optional" : "";
-    ret += "&nbsp;&nbsp;IMPORTING <tt>" + p.getName() + `</tt>${opt}<br>\n`;
+    ret += "&nbsp;&nbsp;IMPORTING <tt>" + p.getName() + `</tt> TYPE <tt>${typeText(p)}</tt>${opt}<br>\n`;
   }
   for (const p of m.getParameters().getChanging()) {
     const opt: string = optional.some(a => a.toUpperCase() === p.getName().toUpperCase()) ? " Optional" : "";
-    ret += "&nbsp;&nbsp;CHANGING <tt>" + p.getName() + `</tt>${opt}<br>\n`;
+    ret += "&nbsp;&nbsp;CHANGING <tt>" + p.getName() + `</tt> TYPE <tt>${typeText(p)}</tt>${opt}<br>\n`;
   }
   for (const p of m.getParameters().getExporting()) {
-    ret += "&nbsp;&nbsp;EXPORTING <tt>" + p.getName() + `</tt><br>\n`;
+    ret += "&nbsp;&nbsp;EXPORTING <tt>" + p.getName() + `</tt> TYPE <tt>${typeText(p)}</tt><br>\n`;
   }
   const returning = m.getParameters().getReturning();
   if (returning) {
-    ret += "&nbsp;&nbsp;RETURNING <tt>" + returning.getName() + "</tt><br>\n";
+    ret += "&nbsp;&nbsp;RETURNING <tt>" + returning.getName() + "</tt> TYPE <tt>" + typeText(returning) + "</tt><br>\n";
+  }
+
+  for (const r of m.getRaising()) {
+    ret += `&nbsp;&nbsp;RAISING <tt>${objectLink("CLAS", r)}</tt><br>\n`;
+  }
+  for (const e of m.getExceptions()) {
+    ret += "&nbsp;&nbsp;EXCEPTIONS <tt>" + e + "</tt><br>\n";
   }
   return ret;
 }
@@ -30,16 +54,16 @@ function outputSection(def: abaplint.IInterfaceDefinition, visibility: abaplint.
     if (t.visibility !== visibility) {
       continue;
     }
-    ret += `Type <tt>${t.type.getName()}</tt><br>\n`;
+    ret += `Type <tt>${t.type.getName()}</tt> TYPE <tt>${typeText(t.type)}</tt><br>\n`;
   }
   for (const a of def.getAttributes().getInstancesByVisibility(visibility)) {
-    ret += `Attribute <tt>${a.getName()}</tt><br>\n`;
+    ret += `Attribute <tt>${a.getName()}</tt> TYPE <tt>${typeText(a)}</tt><br>\n`;
   }
   for (const a of def.getAttributes().getStaticsByVisibility(visibility)) {
-    ret += `Static Attribute <tt>${a.getName()}</tt><br>\n`;
+    ret += `Static Attribute <tt>${a.getName()}</tt> TYPE <tt>${typeText(a)}</tt><br>\n`;
   }
   for (const a of def.getAttributes().getConstantsByVisibility(visibility)) {
-    ret += `Constant <tt>${a.getName()}</tt><br>\n`;
+    ret += `Constant <tt>${a.getName()}</tt> TYPE <tt>${typeText(a)}</tt><br>\n`;
   }
   for (const m of def.getMethodDefinitions().getAll()) {
     if (m.getVisibility() !== visibility) {
